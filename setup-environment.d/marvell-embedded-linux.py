6358abf19dd2b7b8a346a0b7cea9034e788cbe02
Otavio Salvador <otavio@ossystems.com.br>
2016-02-24 13:22:23 -0300

    Initial configuration structure for embedded-linux
    Change-Id: Idda56e6796243bcac222a433af831ff817369282
    Signed-off-by: Otavio Salvador <otavio@ossystems.com.br>

---
+++ setup-environment.d/marvell-embedded-linux.py
@@ -0,0 +1,15 @@
+def __after_init_embedded_linux_yocto():
+    PLATFORM_ROOT_DIR = os.environ['PLATFORM_ROOT_DIR']
+
+    append_layers([ os.path.join(PLATFORM_ROOT_DIR, 'sources', p) for p in
+                    [
+                     'meta-openembedded/meta-python',
+                     'meta-openembedded/meta-oe',
+                     'meta-openembedded/meta-networking',
+                     'meta-marvell-distro',
+                     'meta-marvell'
+                    ]])
+
+
+
+run_after_init(__after_init_embedded_linux_yocto)
