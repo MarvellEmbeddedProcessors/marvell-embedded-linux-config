6ad44cae8d4fce4e8c8e776db869edc46f5730a0
Fabio Berton <fabio.berton@ossystems.com.br>
2016-04-12 09:47:25 -0300

    Add valid_machines to be set on local.conf
    valid_machines are machines that can be built for marvell-embedded-linux.
    Machines that are not in valid_machines can't be set on local.conf.
    
    Machines available now are: clearfog, db-88f6820-gp and db-88f8040-rz-ddr3.
    
    Change-Id: Iea3fb725e4cc52f16ee4b49adae1ad5ff3dd94d1
    Signed-off-by: Fabio Berton <fabio.berton@ossystems.com.br>

--- setup-environment.d/marvell-embedded-linux.py
+++ setup-environment.d/marvell-embedded-linux.py
@@ -1,4 +1,37 @@
 def __set_defaults_marvell_embedded_linux_yocto():
+    import os
+    import sys
+
+    valid_machines = ['clearfog', 'db-88f6820-gp', 'db-88f8040-rz-ddr3']
+
+    local_conf_exists = os.path.isfile(os.path.join(build_dir,
+                                                    'conf',
+                                                    'local.conf'))
+
+    def required_var_error(varname, valid_vals):
+        sys.stderr.write("ERROR: You must set '%s' before setting up the environment.\n" %
+                         (varname,))
+        sys.stderr.write("       Set MACHINE variable with one of the possible values.\n"
+                         "       Possible values are: %s\n\n"
+                         "       Ex: MACHINE=clearfog source setup-environment build\n"
+                         % valid_vals)
+        sys.exit(1)
+
+    def maybe_set_default(varname, valid_vals):
+        try:
+            val = os.environ[varname]
+        except KeyError:
+            val = None
+
+        if val:
+            if val in valid_vals:
+                set_default(varname, val)
+            else:
+                required_var_error(varname, valid_vals)
+        elif not local_conf_exists:
+            required_var_error(varname, valid_vals)
+
+    maybe_set_default('MACHINE', valid_machines)
     set_default('DISTRO', 'marvell')
 
 def __after_init_marvell_embedded_linux_yocto():
